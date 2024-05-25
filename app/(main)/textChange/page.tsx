0a2160b9afaf3ace3a6b6ec4580a0d6ea17ea12a
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-25 16:09:52 +0900

    テキスト表示切替機能実装した

--- app/(main)/textChange/page.tsx
+++ app/(main)/textChange/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+import { useState } from 'react'
 
 const TextChange = () => {
+    const [isHidden, setIsHidden] = useState(false);
+
+    const handleDisplay = () => {
+        setIsHidden(false);
+    }
+
+    const handleDisplayOff = () => {
+        setIsHidden(true);
+    }
+
+    // const handleChange = () => {
+    //     setIsHidden(!isHidden);
+    // }
+
     return (
-        <div>
-            テキスト切り替えプロジェクトです
+        <div className='mx-auto mt-10 max-w-4xl'>
+            <div className='flex justify-center'>
+                <div className='flex flex-col gap-3'>
+                    {
+                        <h2 className={`mb-4 text-3xl ${isHidden ? 'invisible' : 'visible'}`} >
+                            こんにちは！
+                        </h2>
+                    }
+                    <button
+                        onClick={handleDisplay}
+                        className="border-2 border-black px-3 py-2 rounded-lg bg-blue-300">
+                        表示
+                    </button>
+                    <button
+                        onClick={handleDisplayOff}
+                        className="border-2 border-black px-3 py-2 rounded-lg bg-red-300">
+                        非表示
+                    </button>
+                    {/* <button
+                        onClick={handleChange}
+                        className="border-2 border-black px-3 py-2 rounded-lg bg-pink-300">
+                        非表示
+                    </button> */}
+                </div>
+            </div>
         </div>
     )
 }
