620cd00c9eb0955a6762004ce4589bc90c185933
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-25 17:05:49 +0900

    カラーチェンジ機能実装した

--- app/(main)/colorChange/page.tsx
+++ app/(main)/colorChange/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+import { NextPage } from "next"
+import { useState } from "react";
 
-const ButtonColorChange = () => {
+const ButtonColorChange: NextPage = () => {
+    const [color, setColor] = useState('skyblue')
+    const colors = ['lightblue', 'lightgreen', 'pink', 'red', 'indigo', 'green', 'slate', 'orange', 'yellow', 'purple'];
+
+    const handleColorChange = () => {
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        setColor(randomColor);
+    }
+
     return (
-        <div>
-            ボタンカラーチェンジプロジェクトです
+        <div className="h-screen pt-8" style={{ backgroundColor: color }}>
+            <div className='flex justify-center'>
+                <button
+                    onClick={handleColorChange}
+                    className={` border-4 border-gray-500 bg-slate-300 px-4 py-3 rounded-md`}>
+                    カラーチェンジ
+                </button>
+            </div>
         </div>
     )
 }
