e59e2e9a2f69bf4e1b89ef2e2a8db0e7c561ad8f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-29 08:05:38 +0900

    フロント部分に表示させた

--- app/(main)/fibonacci/page.tsx
+++ app/(main)/fibonacci/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect, useRef } from 'react';
+
 const Practice = () => {
   // フィボナッチジェネレータ関数
   function* fibonacci() {
@@ -8,20 +11,46 @@ const Practice = () => {
     }
   }
 
+  const fibRef = useRef<Generator<number, void, unknown>>(fibonacci()); // useRefでジェネレータを保持
+  const [result, setResult] = useState<number>(0); // 初期値を設定
+  const [isRunning, setIsRunning] = useState(false); // タイマーの実行状態を管理
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isRunning) {
+      interval = setInterval(() => {
+        const nextValue = fibRef.current.next().value;
+        if (nextValue !== undefined) {
+          setResult(nextValue);
+        }
+      }, 1000);
+    }
+
+    // コンポーネントがアンマウントされたとき、またはisRunningがfalseになったときにインターバルをクリア
+    return () => clearInterval(interval);
+  }, [isRunning]); // isRunningが変化するたびにエフェクトを再実行
+
+  const start = () => {
+    setIsRunning(true);
+  };
+
+  const stop = () => setIsRunning(false);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="text-6xl mb-10 font-bold text-gray-800 bg-white p-4 rounded-lg shadow-lg border border-gray-200 transform transition-transform duration-300 hover:scale-110">
-        結果
+    <div className="flex flex-col gap-5 items-center justify-center h-screen bg-gray-100">
+      <h1 className='text-4xl font-mono'>Fibonacci</h1>
+      <div className="text-6xl font-bold text-gray-800 bg-white p-4 rounded-lg shadow-lg border border-gray-200 transform transition-transform duration-300 hover:scale-110">
+        {result}
       </div>
       <div className="flex space-x-4 mt-10">
         <button
-          onClick={}
+          onClick={start}
           className="px-6 py-3 text-xl font-bold text-white bg-green-500 rounded hover:bg-green-600 transition"
         >
           Start
         </button>
         <button
-          onClick={}
+          onClick={stop}
           className="px-6 py-3 text-xl font-bold text-white bg-red-500 rounded hover:bg-red-600 transition"
         >
           Stop
