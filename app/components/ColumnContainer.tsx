e85f2eef8d923c8ee9efdd5f5e0f986be868de20
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-30 12:05:28 +0900

    追加されたカラムをDrug&Dropできるようにした

--- app/components/ColumnContainer.tsx
+++ app/components/ColumnContainer.tsx
@@ -1,5 +1,7 @@
+import { useSortable } from "@dnd-kit/sortable";
 import { Column } from "../(main)/drag&drop/types"
 import { DeleteIcon } from "./icon/DeleteIcon";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
     column: Column;
@@ -7,10 +9,39 @@ interface Props {
 }
 export const ColumnContainer = (props: Props) => {
     const { column, deleteColum } = props;
+    const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+        id: column.id,
+        data: {
+            type: "Column",
+            column,
+        }
+    });
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform)
+    }
+
+    if (isDragging) {
+        return (
+            <div ref={setNodeRef} style={style}
+                className="bg-slate-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col
+                opacity-40 border-2 border-rose-500"
+            >
+
+            </div>
+        )
+    }
 
     return (
-        <div className="bg-slate-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
-            <div className="bg-sky-700 h-[60px] cursor-grab rounded-md rounded-b-none p-3
+        <div
+            ref={setNodeRef}
+            style={style}
+            className="bg-slate-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+            <div
+                {...attributes}
+                {...listeners}
+                className="bg-sky-700 h-[60px] cursor-grab rounded-md rounded-b-none p-3
             font-bold border-slate-700 border-4 flex items-center justify-between">
                 <div className="flex gap-2">
                     <div className="flex justify-center items-center bg-slate-700 px-2 py-1 text-sm rounded-full">
--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,13 +1,17 @@
 "use client";
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { PlusIcon } from "./icon/PlusIcon"
 import { Column } from "../(main)/drag&drop/types";
 import { ColumnContainer } from "./ColumnContainer";
+import { DndContext, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 
 export const TaskCard = () => {
 
   const [columns, setColumns] = useState<Column[]>([]);
-  console.log(columns);
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
 
   // column作成ボタン
   const createNewColumn = () => {
@@ -29,22 +33,40 @@ export const TaskCard = () => {
     return Math.floor(Math.random() * 1001);
   };
 
+  const onDragStart = (e: DragStartEvent) => {
+    console.log(e);
+    if (e.active.data.current?.type === "Column") {
+      setActiveColumn(e.active.data.current.column);
+      return;
+    }
+  }
+
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-auto px-[40px] bg-slate-900">
-      <div className="m-auto flex gap-4">
-        <div className="text-white flex gap-2">
-          {columns.map((col) => (
-            <ColumnContainer key={col.id} column={col} deleteColum={deleteColumn} />
-          ))}
+      <DndContext onDragStart={onDragStart}>
+        <div className="m-auto flex gap-4">
+          <div className="text-white flex gap-2">
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <ColumnContainer key={col.id} column={col} deleteColum={deleteColumn} />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={createNewColumn}
+            className="h-[60px] w-[30px] min-w-[350px] cursor-pointer text-white flex gap-2
+          rounded-lg bg-slate-700 border-gray-500 border-2 p-4 ring-rose-500 hover:ring-2">
+            <PlusIcon />
+            カラム追加
+          </button>
         </div>
-        <button
-          onClick={createNewColumn}
-          className="h-[60px] w-[30px] min-w-[350px] cursor-pointer text-white flex gap-2
-        rounded-lg bg-slate-700 border-gray-500 border-2 p-4 ring-rose-500 hover:ring-2">
-          <PlusIcon />
-          カラム追加
-        </button>
-      </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && <ColumnContainer column={activeColumn} deleteColum={deleteColumn} />}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   )
 }
