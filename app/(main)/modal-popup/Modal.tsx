b729607e6fc41aefcff0fcf3301d0a860a379ae7
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-02 10:53:01 +0900

    ボタンにリンクを設定した

--- app/(main)/modal-popup/Modal.tsx
+++ app/(main)/modal-popup/Modal.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
 type Props = {
-    open: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
-export const Modal: React.FC<Props> = ({open, onClose, children}) => {
+export const Modal: React.FC<Props> = ({ open, onClose, children }) => {
   return (
     <div
-    onClick={onClose}
-    className={`fixed inset-0 min-h-screen flex justify-center items-center transition-colors
+      className={`fixed inset-0 min-h-screen flex justify-center items-center transition-colors
     ${open ? "visible bg-black/20" : "invisible"}`}>
-        <div className={`bg-white rounded-lg shadow p-6 transition-all max-w-md
+      <div className={`bg-white rounded-lg shadow p-6 transition-all max-w-md
         ${open ? "scale-100 opacity-100" : "scale-110 opacity-0"}`}>
-            <button
-            onClick={onClose}
-            className='absolute top-2 right-2 px-2 border border-neutral-200 rounded-md
+        <button
+          onClick={onClose}
+          className='absolute top-2 right-2 px-2 border border-neutral-200 rounded-md
              text-gray-600 bg-white hover:bg-gray-50 hover:text-gray-600'>
-            X
-            </button>
-            {children}
-        </div>
+          X
+        </button>
+        {/* このchildrenにはModalタグ内のコンテンツが渡ってくる */}
+        {children}
+      </div>
     </div>
   )
 }
--- app/(main)/modal-popup/page.tsx
+++ app/(main)/modal-popup/page.tsx
@@ -5,32 +5,35 @@ import Link from 'next/link';
 
 const ModalPage = () => {
     const [open, setOpen] = useState<boolean>(false);
- 
-  return (
-    <div className='w-full min-h-screen flex justify-center items-center'>
-        <button
-        onClick={()=>setOpen(true)}
-        className='px-3 py-2 text-white bg-indigo-500 hover:bg-indigo-400 rounded-md shadow-lg'>
-            Modal Open
-        </button>
-        <Modal open={open} onClose={() => setOpen(false)} >
-            <div className='flex flex-col gap-4 items-center'>
-                <h1 className='text-2xl font-mono'>ポケモン神経衰弱ゲーム!!</h1>
-                <p className='text-sm font-mono'>ポケモンAPIを活用した神経衰弱ゲームが体験できます</p>
-                <div className='flex items-center justify-center gap-3'>
-                    <Link href={"/pokemon-game"}>
-                        <button className="bg-green-500 px-3 py-2 rounded-lg text-white hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0">
-                            ゲームを開始する
-                        </button>
-                    </Link>
-                    <button className="bg-red-400 px-3 py-2 rounded-lg text-white hover:bg-red-500/90 border-red-600 border-b-4 active:border-b-0">
-                        やめておく
-                    </button>
+
+    return (
+        <div className='w-full min-h-screen flex justify-center items-center'>
+            <button
+                onClick={() => setOpen(true)}
+                className="bg-indigo-500 px-4 py-3 rounded-lg text-white hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0">
+                Modal Open
+            </button>
+            <Modal open={open} onClose={() => setOpen(false)} >
+                <div className='flex flex-col gap-4 items-center'>
+                    <h1 className='text-2xl font-mono'>ポケモン神経衰弱ゲーム!!</h1>
+                    <p className='text-sm font-mono'>ポケモンAPIを活用した神経衰弱ゲームが体験できます</p>
+                    <div className='flex items-center justify-center gap-3'>
+                        <Link href={"/pokemon-game"}>
+                            <button className="bg-green-500 px-3 py-2 rounded-lg text-white hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0">
+                                ゲームを開始する
+                            </button>
+                        </Link>
+                        <Link href={"/"}>
+                            <button
+                                className="bg-red-400 px-3 py-2 rounded-lg text-white hover:bg-red-500/90 border-red-600 border-b-4 active:border-b-0">
+                                やめておく
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </Modal>
-    </div>
-  )
+            </Modal>
+        </div>
+    )
 }
 
 export default ModalPage
