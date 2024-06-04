09c36964070825b8f27222f79d33e61cff825b3f
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-04 11:50:12 +0900

    modalSlice実装してstoreに登録した

--- app/(main)/redux-toolkit/store.ts
+++ app/(main)/redux-toolkit/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/CartSlice";
+import ModalSlice from "./features/modal/ModalSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: ModalSlice,
   },
 });
 
