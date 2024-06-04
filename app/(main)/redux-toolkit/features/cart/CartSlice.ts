89ab77b58d50bfc96961288bda08c6cbc5550f7a
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-04 11:37:59 +0900

    cartSlice実装してstoreに登録した

--- app/(main)/redux-toolkit/features/cart/CartSlice.ts
+++ app/(main)/redux-toolkit/features/cart/CartSlice.ts
@@ -1,8 +1,55 @@
-import { createSlice, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CartState } from "../../types";
+import { Products } from "../../data/Products";
 
 const initialState: CartState = {
-  cartItems: [],
+  cartItems: Products,
   amount: 1,
   total: 0,
 };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+      state.amount = 0;
+      state.total = 0;
+    },
+    removeItem: (state, action: PayloadAction<number>) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action: PayloadAction<number>) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      if (cartItem) {
+        cartItem.amount += 1;
+      }
+    },
+    decrease: (state, action: PayloadAction<number>) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      if (cartItem && cartItem.amount > 1) {
+        cartItem.amount -= 1;
+      }
+    },
+    calculateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+});
+
+export const { clearCart, increase, decrease, calculateTotal } =
+  cartSlice.actions;
+export default cartSlice.reducer;
