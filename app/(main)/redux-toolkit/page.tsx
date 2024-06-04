6e998fd0ca69e49203824ee5703df057fdce50e7
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-04 12:02:14 +0900

    メインページに使用する機能とコンポーネントの設定をした

--- app/(main)/redux-toolkit/page.tsx
+++ app/(main)/redux-toolkit/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Modal from './components/Modal'
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+import { useDispatch, useSelector } from 'react-redux'
+import { calculateTotal } from './features/cart/CartSlice'
+import { RootState } from './store'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state: RootState) => state.cart.cartItems);
+    const isOpen = useSelector((state: RootState) => state.modal.isOpen);
+
+    useEffect(() => {
+        dispatch(calculateTotal());
+    }, [cartItems, dispatch]);
+
     return (
-        <div>
-            Redux Toolkit Project
-        </div>
+        <main>
+            {isOpen && <Modal />}
+            <Navbar />
+            <CartContainer />
+        </main>
     )
 }
 
