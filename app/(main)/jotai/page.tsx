300cb62ffedf4e7c662212decc62716c517d8d3c
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-10-20 13:59:46 +0900

    カートに追加機能実装

--- app/(main)/jotai/page.tsx
+++ app/(main)/jotai/page.tsx
@@ -3,6 +3,21 @@ import { cartAtom, type CartItem } from "./utils/CartAtom";
 
 export default function CartPage() {
     const [cartItem, setCartItem] = useAtom(cartAtom);
+
+    // カートにアイテム追加する機能
+    const addItem = (item: CartItem) => {
+        setCartItem((prevCart) => {
+                const existingItem = prevCart.find((prevItem) => prevItem.id === item.id);
+                if(existingItem) {
+                    return prevCart.map((prevCartItem) => (
+                        prevCartItem.id === item.id
+                    ) ? { ...prevCartItem, quantity: item.quantity + 1} : prevCartItem
+                );
+            }
+            return [...prevCart, {...item, quantity: item.quantity + 1}];
+        });
+    }
+    
   return (
     <div>
       
