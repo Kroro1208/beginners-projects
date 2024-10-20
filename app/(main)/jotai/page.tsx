4adafcf6de63310a97de9c2fbc34b8eba95c1d8f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-10-20 14:44:56 +0900

    装飾

--- app/(main)/jotai/page.tsx
+++ app/(main)/jotai/page.tsx
@@ -35,20 +35,22 @@ export default function CartPage() {
     }, 0)
     
   return (
-    <div>
-      <h1>Shopping Cart🛒</h1>
-      <p>カートの中身の合計{totalAmount}</p>
-      <div>
+    <div className="container mx-auto py-10 px-4 lg:px-8">
+      <h1 className="text-4xl font-bold text-center mb-10">Shopping Cart 🛒</h1>
+      <div className="flex justify-center mb-8">
+        <p className="text-xl font-semibold text-gray-800">カートの中身の合計: <span className="text-green-500">{totalAmount}</span> 円</p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {products.map((product) => (
-            <Card key={product.id}>
+            <Card key={product.id} className="shadow-lg hover:shadow-xl transition-shadow duration-300">
                 <CardHeader>
-                    <CardTitle>{product.name}</CardTitle>
+                    <CardTitle className="text-2xl font-medium">{product.name}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <p>値段: {product.price}</p>
-                    <Badge>季節の果物</Badge>
+                    <p className="text-gray-700">値段: {product.price}</p>
+                    <Badge variant="outline" className="mt-2">季節の果物</Badge>
                 </CardContent>
-                <CardFooter>
+                <CardFooter className="flex justify-center">
                     <Button variant="default"  onClick={() => addItem(product)}>
                         カートに追加
                     </Button>
@@ -56,14 +58,14 @@ export default function CartPage() {
             </Card>
         ))}
       </div>
-      <div>
-        <h3>カートの中身</h3>
-        <ul>
+      <div className="mt-12">
+        <h3 className="text-3xl font-bold mb-6">カートの中身</h3>
+        <ul className="space-y-4">
             {cartItem.map((item) => (
-                <li key={item.id}>
+                <li key={item.id} className="flex justify-between items-center p-4 border rounded-lg shadow-md">
                     <div>
-                        <span>{item.name}</span>
-                        <span>価格: {item.price * item.quantity}/数量: {item.quantity}</span>
+                        <span className="font-medium text-lg">{item.name}</span>
+                        <span className="ml-4 text-gray-700">価格: {item.price * item.quantity}/数量: {item.quantity}</span>
                     </div>
                     <Button variant="destructive" onClick={() => removeItem(item.id)}>
                         削除
